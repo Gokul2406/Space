da24f200beb0af939e6f6487380f30ffbf94e7b6
Gokul <gokulpbharathan89@gmail.com>
2025-12-25 20:12:35 +0530

    Updated Files

--- src/config.ts
+++ src/config.ts
@@ -3,18 +3,18 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
-    description: 'Minimal blog built by Astro', // Site description
+    website: 'https://gokul2406.github.io/blog/', // Site domain
+    title: "Gokul's Space", // Site title
+    author: 'Gokul', // Author name
+    description: 'A space to host my blogs and similar content', // Site description
     language: 'en-US' // Default language
   },
 
   // GENERAL SETTINGS ////////////////////////////////////////////////////////////////////////////////////
   general: {
-    contentWidth: '35rem', // Content area width
+    contentWidth: '70rem', // Content area width
     centeredLayout: true, // Use centered layout (false for left-aligned)
-    themeToggle: false, // Show theme toggle button (uses system theme by default)
+    themeToggle: true, // Show theme toggle button (uses system theme by default)
     postListDottedDivider: false, // Show dotted divider in post list
     footer: true, // Show footer
     fadeAnimation: true // Enable fade animations
@@ -22,17 +22,17 @@ export const themeConfig: ThemeConfig = {
 
   // DATE SETTINGS ///////////////////////////////////////////////////////////////////////////////////////
   date: {
-    dateFormat: 'YYYY-MM-DD', // Date format: YYYY-MM-DD, MM-DD-YYYY, DD-MM-YYYY, MONTH DAY YYYY, DAY MONTH YYYY
-    dateSeparator: '.', // Date separator: . - / (except for MONTH DAY YYYY and DAY MONTH YYYY)
+    dateFormat: 'DD-MM-YYYY', // Date format: YYYY-MM-DD, MM-DD-YYYY, DD-MM-YYYY, MONTH DAY YYYY, DAY MONTH YYYY
+    dateSeparator: '-', // Date separator: . - / (except for MONTH DAY YYYY and DAY MONTH YYYY)
     dateOnRight: true // Date position in post list (true for right, false for left)
   },
 
   // POST SETTINGS ///////////////////////////////////////////////////////////////////////////////////////
   post: {
-    readingTime: false, // Show reading time in posts
+    readingTime: true, // Show reading time in posts
     toc: true, // Show table of contents (when there is enough page width)
-    imageViewer: true, // Enable image viewer
+    imageViewer: true, // Eable image viewer
     copyCode: true, // Enable copy button in code blocks
-    linkCard: true // Enable link card
+    linkCard: false // Enable link card
   }
 }
--- src/content/about/about.md
+++ src/content/about/about.md
@@ -2,15 +2,6 @@
 title: 'About'
 ---
 
-<!--
-This content will be displayed at the top of the index page.
-You can leave this empty if you don’t want to show any content.
--->
+This is a webpage which is used for hosting my blogs. Now blogs here is used in an umbrella setting in the sense that I plan to cover post travelogues, random thoughts, maybe some poetry, technical guides etc. 
 
-A static blog theme based on [Astro](https://astro.build), designed for clarity and focus.
-
-With a deliberately minimal design, this layout ensures your content takes center stage. It's built for flexibility, offering customization options that honor its clean and elegant aesthetic.
-
-Effortlessly share your thoughts in _a calm & dustless space._
-
-Check posts for details and view source on [GitHub](https://github.com/the3ash/astro-chiri).
+This was built using the Chiri theme. Check out it's [Github](https://github.com/the3ash/astro-chiri)
